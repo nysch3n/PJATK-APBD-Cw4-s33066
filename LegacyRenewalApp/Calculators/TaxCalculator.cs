574b7caa931f46a177043ad03b3d4ced41dbaeca
Jakub Rębkowski <s33066@pjwstk.edu.pl>
2026-04-07 17:24:01 +0200

    wydzielenie logiki podatkowej do TaxCalculator

---
+++ LegacyRenewalApp/Calculators/TaxCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace LegacyRenewalApp.Calculators;
+
+public interface ITaxCalculator
+{
+    decimal GetTaxRate(string country);
+}
+
+public class TaxCalculator : ITaxCalculator
+{
+    public decimal GetTaxRate(string country) => country switch
+    {
+        "Poland" => 0.23m,
+        "Germany" => 0.19m,
+        "Czech Republic" => 0.21m,
+        "Norway" => 0.25m,
+        _ => 0.20m
+    };
+}
