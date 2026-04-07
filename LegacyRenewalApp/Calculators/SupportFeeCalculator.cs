e667489f349053a084cc9f7803ab7bb217c84e32
Jakub Rębkowski <s33066@pjwstk.edu.pl>
2026-04-07 17:42:16 +0200

    wydzielenie kosztow wsparcia

---
+++ LegacyRenewalApp/Calculators/SupportFeeCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace LegacyRenewalApp.Calculators;
+
+public interface ISupportFeeCalculator
+{
+    (decimal Fee, string Note) CalculateFee(string planCode, bool includeSupport);
+}
+
+public class SupportFeeCalculator : ISupportFeeCalculator
+{
+    public (decimal Fee, string Note) CalculateFee(string planCode, bool includeSupport)
+    {
+        if (!includeSupport) return (0m, string.Empty);
+
+        decimal fee = planCode switch
+        {
+            "START" => 250m,
+            "PRO" => 400m,
+            "ENTERPRISE" => 700m,
+            _ => 0m
+        };
+        return (fee, "premium support included; ");
+    }
+}
