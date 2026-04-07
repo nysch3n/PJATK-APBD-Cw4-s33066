23037d8c7216754a7a07f5e79189695fe34c7678
Jakub Rębkowski <s33066@pjwstk.edu.pl>
2026-04-07 17:32:47 +0200

    wydzielenie logiki oplat

---
+++ LegacyRenewalApp/Calculators/PaymentFeeCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LegacyRenewalApp.Calculators;
+
+public interface IPaymentFeeCalculator
+{
+    (decimal Fee, string Note) CalculateFee(string paymentMethod, decimal baseAmountWithSupport);
+}
+
+public class PaymentFeeCalculator : IPaymentFeeCalculator
+{
+    public (decimal Fee, string Note) CalculateFee(string paymentMethod, decimal baseAmountWithSupport)
+    {
+        return paymentMethod switch
+        {
+            "CARD" => (baseAmountWithSupport * 0.02m, "card payment fee; "),
+            "BANK_TRANSFER" => (baseAmountWithSupport * 0.01m, "bank transfer fee; "),
+            "PAYPAL" => (baseAmountWithSupport * 0.035m, "paypal fee; "),
+            "INVOICE" => (0m, "invoice payment; "),
+            _ => throw new ArgumentException("Unsupported payment method")
+        };
+    }
+}
