f976622d10f477560f0783492d3e4deb6bcb0a6b
Jakub Rębkowski <s33066@pjwstk.edu.pl>
2026-04-07 17:19:37 +0200

    opakowanie zewnetrznej klasy statycznej

---
+++ LegacyRenewalApp/Infrastructure/BillingGatewayAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace LegacyRenewalApp.Infrastructure;
+
+public interface IBillingGatewayAdapter
+{
+    void SaveInvoice(RenewalInvoice invoice);
+    void SendEmail(string email, string subject, string body);
+}
+
+public class BillingGatewayAdapter : IBillingGatewayAdapter
+{
+    public void SaveInvoice(RenewalInvoice invoice) => LegacyBillingGateway.SaveInvoice(invoice);
+        
+    public void SendEmail(string email, string subject, string body) => LegacyBillingGateway.SendEmail(email, subject, body);
+}
